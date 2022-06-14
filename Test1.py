bea4b814ab97b1b959f8bec455d730873e23828b
Minh <dminhnguyen161@gmail.com>
2022-06-14 12:58:11 -0400

    Testing commit

--- Test1.py
+++ Test1.py
@@ -1,1 +1,19 @@
-import pygame
+import random
+# GLOBAL VARIABLES
+s = 2 #NUMBER OF TUPLES TO SHOW
+d = 2 #NUMBER OF ATTRIBUTES TO SHOW
+
+
+def PolyFunction(lowestDeg, highestDeg, n=2):
+    function = [[random.random(),random.randint(lowestDeg,highestDeg)] for i in range(n)]
+    
+
+    return function
+
+
+
+def main():
+    print(PolyFunction(1,10))
+
+if __name__ == "__main__":
+    main()
